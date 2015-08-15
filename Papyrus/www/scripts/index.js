2e619bf4dd8986266c9f575af629246fbef8a954
Fred Buchanan <fred@pineapplecomputerart.com>
2015-08-15 11:12:16 -0400

    Saves notes now

--- Papyrus/www/scripts/index.js
+++ Papyrus/www/scripts/index.js
@@ -2,6 +2,8 @@
 // http://go.microsoft.com/fwlink/?LinkID=397704
 // To debug code on page load in Ripple or on Android devices/emulators: launch your app, set breakpoints, 
 // and then run "window.location.reload()" in the JavaScript Console.
+
+
 (function () {
     "use strict";
 
@@ -11,8 +13,6 @@
         // Handle the Cordova pause and resume events
         document.addEventListener( 'pause', onPause.bind( this ), false );
         document.addEventListener( 'resume', onResume.bind( this ), false );
-        
-        // TODO: Cordova has been loaded. Perform any initialization that requires Cordova here.
     };
 
     function onPause() {
@@ -22,4 +22,4 @@
     function onResume() {
         // TODO: This application has been reactivated. Restore application state here.
     };
-} )();
+})();
---
+++ Papyrus/www/scripts/noteApp.js
@@ -0,0 +1,14 @@
+﻿noteApp = angular.module('noteApp', []);
+noteApp.controller('noteCtrl', function ($scope) {
+    $scope.text = localStorage.getItem("note");
+    $scope.save = function () {
+        console.log("saving");
+        localStorage.setItem("note", $scope.text);
+    }
+    $scope.clear = function () {
+        console.log("clearing")
+        $scope.text = "";
+        $scope.save();
+    }
+
+});
